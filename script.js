8fe847997c01b7ce535ebf6d3c7e6cf368382c56
VolodymyrVorob <v.vorobets2011@gmail.com>
2025-04-25 16:54:27 +0300

    maybe final1

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let byeBtn = document.querySelector(".buy-btn");  // Кнопка відкрит
 let modal = document.getElementById("myModal");       // Модальне вікно
 let overlay = document.getElementById("overlay");     // Затемнений фон
 let closeBtn = document.querySelector(".close");      // Кнопка закриття
-
+let getbtn = document.querySelector(".modal-button");
+let modaltitle = document.querySelector(".modal-title")
+let input = document.querySelector(".myname");
 
 let selectedCards = {
     'shatls-block': null,
@@ -28,6 +30,13 @@ sections.forEach((section) => {
 //array and object
 
 byeBtn.addEventListener("click",() => {
+    modal.querySelector('.room-name').style.display = "block";
+    modal.querySelector('.delivery-name').style.display = "block";
+    modal.querySelector('.souvenirs-name').style.display = "block";
+    getbtn.style.display = "initial";
+    modaltitle.style.display = "initial";
+    input.style.display = "initial";
+    modal.querySelector('.shatl-name').classList.remove("thanks");
 modal.querySelector('.shatl-name').innerHTML = ("Шатл:&nbsp;&nbsp;&nbsp;" + selectedCards["shatls-block"].querySelector("span").innerHTML)
 modal.querySelector('.room-name').innerHTML = ("Каюта:&nbsp;&nbsp;&nbsp;" + selectedCards["room-block"].querySelector("span").innerHTML)
 modal.querySelector('.delivery-name').innerHTML = ("Доставка:&nbsp;&nbsp;&nbsp;" + selectedCards["delivery-block"].querySelector("span").innerHTML)
@@ -56,3 +65,13 @@ overlay.addEventListener("click", () => {
     modal.style.display = "none";
     overlay.style.display = "none";
 });
+getbtn.addEventListener("click", () => {
+    modal.querySelector('.room-name').style.display = "none";
+    modal.querySelector('.delivery-name').style.display = "none";
+    modal.querySelector('.souvenirs-name').style.display = "none";
+    getbtn.style.display = "none";
+    modaltitle.style.display = "none";
+    modal.querySelector('.shatl-name').innerHTML = ("Дякую за замовлення!")
+    modal.querySelector('.shatl-name').classList.add("thanks")  
+    input.style.display = "none";
+})
